933dd34545ffe4bb7d88e34037cab0934797b61c
Sangkon Han <sigmadream@gmail.com>
2021-05-26 22:30:21 +0900

    Dash and Plotly

--- main.py
+++ main.py
@@ -1,11 +1,85 @@
-import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+from data import countries_df, totals_df
+from builders import make_table
 
-daily_df = pd.read_csv("data/daily_report.csv")
+stylesheets = [
+    "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
+    "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap",
+]
 
-total_df = daily_df[["Confirmed", "Deaths", "Recovered"]].sum().reset_index(name="count")
-total_df = total_df.rename(columns={"index": "condition"})
-print(total_df)
+app = dash.Dash(__name__, external_stylesheets=stylesheets)
 
-countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
-countries_df = countries_df.groupby("Country_Region").sum().reset_index()
-print(countries_df)
+bubble_map = px.scatter_geo(
+    countries_df,
+    size="Confirmed",
+    hover_name="Country_Region",
+    color="Confirmed",
+    locations="Country_Region",
+    locationmode="country names",
+    size_max=40,
+    title="Confirmed By Country",
+    template="plotly_dark",
+    color_continuous_scale=px.colors.sequential.Oryel,
+    hover_data={
+        "Confirmed": ":,",
+        "Deaths": ":,",
+        "Recovered": ":,",
+        "Country_Region": False,
+    },
+)
+bubble_map.update_layout(margin=dict(l=0, r=0, t=50, b=0))
+
+bars_graph = px.bar(
+    totals_df,
+    x="condition",
+    hover_data={"count": ":,"},
+    y="count",
+    template="plotly_dark",
+    title="Total Global Cases",
+    labels={"condition": "Condition", "count": "Count", "color": "Condition"},
+)
+bars_graph.update_traces(marker_color=["#e74c3c", "#8e44ad", "#27ae60"])
+
+app.layout = html.Div(
+    style={
+        "minHeight": "100vh",
+        "backgroundColor": "#111111",
+        "color": "white",
+        "fontFamily": "Open Sans, sans-serif",
+    },
+    children=[
+        html.Header(
+            style={"textAlign": "center", "paddingTop": "50px", "marginBottom": 100},
+            children=[html.H1("코로나19 대쉬보드", style={"fontSize": 40})],
+        ),
+        html.Div(
+            style={
+                "display": "grid",
+                "gap": 50,
+                "gridTemplateColumns": "repeat(4, 1fr)",
+            },
+            children=[
+                html.Div(
+                    style={"grid-column": "span 3"},
+                    children=[dcc.Graph(figure=bubble_map)],
+                ),
+                html.Div(children=[make_table(countries_df)]),
+            ],
+        ),
+        html.Div(
+            style={
+                "display": "grid",
+                "gap": 50,
+                "gridTemplateColumns": "repeat(4, 1fr)",
+            },
+            children=[html.Div(children=[dcc.Graph(figure=bars_graph)]),],
+        ),
+    ],
+)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
